63108b37c13793eb2fdaef66ed16bff9f1c5a0fc
Inaki De Tejada Plessmann <inaki.de.tejada@lunatech.com>
2017-02-26 14:27:10 +0100

    Users API implementation

--- src/main/scala/com/lunatech/skills/UserApi.scala
+++ src/main/scala/com/lunatech/skills/UserApi.scala
@@ -2,11 +2,13 @@ package com.lunatech.skills
 
 import java.util.UUID
 
-import com.lunatech.skills.Routes._
 import com.lunatech.skills.models.{User, UserNotFound}
 import com.twitter.finagle.stats.Counter
 import com.twitter.logging.Logger
+import io.circe.generic.auto._
 import io.finch._
+import io.finch.circe._
+import Routes._
 
 /**
   * Created by erikjanssen on 12/02/2017.
@@ -17,21 +19,57 @@ trait UserApi {
 
   val usersCounter: Counter
 
-  def getUser: Endpoint[User] = ???
+  def getUser: Endpoint[User] =
+    get(users :: uuid) { id: UUID =>
+      log.info("serving get user request")
+      User.get(id) match {
+        case Some(u) => Ok(u)
+        case None    => NotFound(UserNotFound(id))
+      }
+    }
 
-  def postedUser: Endpoint[User] = ???
+  def postedUser: Endpoint[User] = body.as[UUID => User].map(_(UUID.randomUUID()))
 
-  def postUser: Endpoint[User] = ???
+  def postUser: Endpoint[User] = post(users :: postedUser) { u: User =>
+    {
+      usersCounter.incr()
+      User.save(u)
+      Created(u)
+    }
+  }
 
-  def patchedUser: Endpoint[User => User] = ???
+  def patchedUser: Endpoint[User => User] = body.as[User => User]
 
-  def patchUser: Endpoint[User] = ???
+  def patchUser: Endpoint[User] =
+    patch(users :: uuid :: patchedUser) { (id: UUID, pt: User => User) =>
+      User.get(id) match {
+        case Some(currentUser) =>
+          val newUser: User = pt(currentUser)
+          User.delete(id)
+          User.save(newUser)
 
-  def getUsers: Endpoint[List[User]] = ???
+          Ok(newUser)
+        case None => throw UserNotFound(id)
+      }
+    }
 
-  def deleteUser: Endpoint[User] = ???
+  def getUsers: Endpoint[List[User]] = get(users) {
+    Ok(User.list())
+  }
 
-  def deleteUsers: Endpoint[List[User]] = ???
+  def deleteUser: Endpoint[User] = delete(users :: uuid) { id: UUID =>
+    User.get(id) match {
+      case Some(t) => User.delete(id); Ok(t)
+      case None    => throw UserNotFound(id)
+    }
+  }
+
+  def deleteUsers: Endpoint[List[User]] = delete(users) {
+    val all: List[User] = User.list()
+    all.foreach(t => User.delete(t.id))
+
+    Ok(all)
+  }
 
   val userApi = getUser :+: getUsers :+: postUser :+: deleteUser :+: deleteUsers :+: patchUser
 
--- src/main/scala/com/lunatech/skills/models/User.scala
+++ src/main/scala/com/lunatech/skills/models/User.scala
@@ -2,12 +2,34 @@ package com.lunatech.skills.models
 
 import java.util.UUID
 
+import io.circe.{Decoder, Encoder, Json}
+import io.circe.syntax._
+
 /**
   * Created by tatianamoldovan on 05/02/2017.
   */
 case class User(id: UUID, firstName: String, lastName: String, email: String) extends Model
 
-object User extends DB[User] {}
+object User extends DB[User] {
+
+  implicit val encodeUser: Encoder[User] = new Encoder[User] {
+    override def apply(skill: User): Json = Json.obj(
+      "id"        -> skill.id.asJson,
+      "firstName" -> skill.firstName.asJson,
+      "lastName"  -> skill.lastName.asJson,
+      "email"     -> skill.email.asJson
+    )
+  }
+
+  implicit val decodeUser: Decoder[User] = Decoder.instance(c =>
+    for {
+      id        <- c.downField("id").as[UUID]
+      firstName <- c.downField("firstName").as[String]
+      lastName  <- c.downField("lastName").as[String]
+      email     <- c.downField("skillType").as[String]
+    } yield User(id, firstName, lastName, email))
+
+}
 
 case class UserSkills(id: UUID, skills: Set[Skill]) extends Model
 
